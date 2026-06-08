ce00f9a406d76a98debbed3fb7fe2ff9e9f4e1a1
next.sukhorutchenko <next.sukhorutchenko@gmail.com>
2026-06-08 20:09:24 +0300

    Use machine UUID in web response

--- modules/application/user_data.sh
+++ modules/application/user_data.sh
@@ -3,38 +3,20 @@ set -euxo pipefail
 
 dnf install -y httpd
 
-cat > /etc/httpd/conf.d/app.conf <<'CONF'
-KeepAlive Off
-MaxKeepAliveRequests 1
-
-<Directory "/var/www/html">
-    Options +ExecCGI
-    AddHandler cgi-script .cgi
-    DirectoryIndex index.cgi
-    Require all granted
-</Directory>
-CONF
-
-cat > /var/www/html/index.cgi <<'CGI'
-#!/bin/bash
-echo "Content-Type: text/html"
-echo "Connection: close"
-echo ""
-
 COMPUTE_MACHINE_UUID=$(cat /sys/devices/virtual/dmi/id/product_uuid | tr '[:upper:]' '[:lower:]')
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 COMPUTE_INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
-RESPONSE_UUID=$(cat /proc/sys/kernel/random/uuid)
 
-cat <<HTML
+cat > /var/www/html/index.html <<HTML
 <html>
   <body>
-    <p>This message was generated on instance ${COMPUTE_INSTANCE_ID} with the following UUID ${RESPONSE_UUID}</p>
+    <p>This message was generated on instance ${COMPUTE_INSTANCE_ID} with the following UUID ${COMPUTE_MACHINE_UUID}</p>
   </body>
 </html>
 HTML
-CGI
 
-chmod 755 /var/www/html/index.cgi
-systemctl enable httpd
-systemctl start httpd
+cat > /etc/httpd/conf.d/keepalive.conf <<'CONF'
+KeepAlive Off
+MaxKeepAliveRequests 1
+CONF
+systemctl enable --now httpd
